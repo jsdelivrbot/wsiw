27f9f0e84337bd8e2b118428a97de08e2ab8f378
Matthew Holman <mateoholman@gmail.com>
2017-02-03 19:42:58 -0600

    Added axios. Setup action for API call

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+const API_KEY = 'b09dc8328902dbe4a4495017081ab633';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export class fetchWeather(city) {
+
+    //Compose our URL according to the API requirements
+    const URL = `${ROOT_URL}&q=${city},us`;
+    //Create a variable for the API call
+    const request = axios.get(url);
+
+    return{
+      type: FETCH_WEATHER,
+      payload: request
+    };
+  }
+}
