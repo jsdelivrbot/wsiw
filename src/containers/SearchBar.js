6d12e66422b2b8a0c87848e3927127d6281f2b36
Matthew Holman <mateoholman@gmail.com>
2017-04-26 21:56:34 -0500

    Changed the SearchBar component default text

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -7,7 +7,7 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state={ term: ''};
+    this.state={ term: 'Search for a city...'};
   }
 
   onInputChange(event){
